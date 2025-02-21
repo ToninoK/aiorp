05449816beeb0e813900725901659d5f49f475aa
ToninoK <tonino.kastelan@kiwi.com>
2025-02-21 15:45:02 +0100

    document code

--- proxy/handler.py
+++ proxy/handler.py
@@ -2,7 +2,7 @@ import asyncio
 from bisect import insort
 from collections import defaultdict
 from enum import IntEnum
-from typing import Callable
+from typing import Any, Awaitable, Callable, List
 
 from aiohttp import ClientResponseError, client, web
 from aiohttp.web_exceptions import HTTPInternalServerError
@@ -12,9 +12,11 @@ from proxy.request import ProxyRequest
 from proxy.response import ProxyResponse, ResponseType
 
 ErrorHandler = Callable[[ClientResponseError], None]
+BeforeHandler = Callable[[ProxyRequest], Awaitable]
+AfterHandler = Callable[[ProxyResponse], Awaitable]
 
 
-class HandlerPriority(IntEnum):
+class Priority(IntEnum):
     """Handler priority enumeration"""
 
     HIGHEST = 0
@@ -24,32 +26,50 @@ class HandlerPriority(IntEnum):
     LOWEST = 4
 
 
-class HandlerCollection:
-    """Dict-like object for storing handlers with priorities"""
+class PriorityCollection:
+    """Dict-like object for storing items with priorities
+
+    The collection stores items while keeping track of their priority. Items with the
+    same priority are stored in a list. Iterating over the collection will yield the
+    lists of items in the order of their priority.
+    """
 
     def __init__(self):
         self._handlers = defaultdict(list)
         self._priorities = []
 
-    def __getitem__(self, priority: HandlerPriority):
+    def __getitem__(self, priority: Priority) -> List:
         return self._handlers[priority]
 
-    def __setitem__(self, priority: HandlerPriority, value):
-        self._handlers[priority] = value
+    def add(self, priority: Priority, value: Any):
+        self._handlers[priority].append(value)
         insort(self._priorities, priority)
 
     def __iter__(self):
+        """Iterate over the priorities in the collection"""
         for priority in self._priorities:
-            yield from self._handlers[priority]
+            yield priority
+
+    def values(self):
+        """Iterate over the values in the collection in the order of their priority"""
+        for priority in self._priorities:
+            yield self._handlers[priority]
+
+    def keys(self):
+        """Iterate over the priorities the collection"""
+        for priority in self._priorities:
+            yield priority
 
     def items(self):
+        """Iterate over the items in the collection"""
         for priority in self._priorities:
             yield priority, self._handlers[priority]
 
     def merge(
         self,
-        handler_collection: "HandlerCollection",
+        handler_collection: "PriorityCollection",
     ):
+        """Merge another collection into this collection"""
         for (
             priority,
             handlers,
@@ -64,18 +84,21 @@ class ProxyHandler:
     This handler is used to proxy requests to a remote server.
     It has a __call__ method that is used to handle incoming requests.
     The handler can be used as a route handler in an aiohttp.web application.
-    It executes specified before and after handlers before and after the
+    It executes specified before and after handlers, before and after the
     incoming request is proxied.
 
     :param proxy_options: The options to use when proxying requests
-    :param rewrite_from: The path to rewrite from
-    :param rewrite_to: The path to rewrite to
-    :param request_options: Additional options to pass to the handler.
-        These options are passed as kwargs to the ProxyRequest.execute method,
-        which is essentially a wrapper over aiohttp.ClientSession.request method.
-        In simple terms these options are passed to the aiohttp request that will
-        be made to the target server.
-    :param error_handler: A callable that is called when an error occurs during the request
+        Defines the URL to proxy requests to and the session to use. It can be None at init, but
+        it must be set before attempting to proxy a request.
+    :param rewrite_from: The path to rewrite from, if specified rewrite_to must also be set
+    :param rewrite_to: The path to rewrite to, if specified rewrite_from must also be set
+    :param error_handler: Callable that is called when an error occurs during the proxied request
+    :param request_options: Additional options for making the request.
+        The specified options are passed to the aiohttp `ClientSession.request` method.
+        The options can't contain the following keys: method, url, headers, params, data.
+        Those parameters are to be managed through the ProxyRequest object in the before handlers.
+
+    :raises: ValueError
     """
 
     def __init__(
@@ -83,8 +106,8 @@ class ProxyHandler:
         proxy_options: ProxyOptions = None,
         rewrite_from=None,
         rewrite_to=None,
-        request_options: dict = None,
         error_handler: ErrorHandler = None,
+        request_options: dict = None,
     ):
 
         if (rewrite_from and rewrite_to is None) or (
@@ -113,10 +136,21 @@ class ProxyHandler:
         self._error_handler = error_handler
 
         self.request_options = request_options or {}
-        self.before_handlers = HandlerCollection()
-        self.after_handlers = HandlerCollection()
+        self.before_handlers = PriorityCollection()
+        self.after_handlers = PriorityCollection()
 
-    async def __call__(self, request: web.Request):
+    async def __call__(self, request: web.Request) -> web.StreamResponse | web.Response:
+        """Handle incoming requests
+
+        This method is called when the handler is used as a route handler in an aiohttp.web app.
+        It executes the before handlers, proxies the request, and executes the after handlers.
+        If an error occurs during the external request, the error_handler is invoked if set,
+        otherwise an HTTPInternalServerError is raised.
+
+        :param request: The incoming request to proxy
+        :returns: The response from the external server
+        :raises: ValueError, HTTPInternalServerError
+        """
         if self._proxy_options is None:
             raise ValueError("Proxy options must be set before the handler is invoked.")
         proxy_request = ProxyRequest(
@@ -131,7 +165,7 @@ class ProxyHandler:
                 self._rewrite_to,
             )
 
-        for handlers in self.before_handlers:
+        for handlers in self.before_handlers.values():
             await asyncio.gather(*(handler(proxy_request) for handler in handlers))
 
         resp = await proxy_request.execute(
@@ -145,7 +179,7 @@ class ProxyHandler:
             in_resp=resp,
             proxy_attributes=self._proxy_options.attributes,
         )
-        for handlers in self.after_handlers:
+        for handlers in self.after_handlers.values():
             await asyncio.gather(*(handler(proxy_response) for handler in handlers))
 
         if not proxy_response.response:
@@ -154,6 +188,15 @@ class ProxyHandler:
         return proxy_response.response
 
     def _raise_for_status(self, response: client.ClientResponse):
+        """Check status of request and handle the error properly
+
+        In case of an error, the error_handler is called if set, otherwise an
+        HTTPInternalServerError is raised with the error message.
+
+        :param response: The response from the external server
+        :returns: None
+        :raises: HTTPInternalServerError
+        """
         try:
             response.raise_for_status()
         except ClientResponseError as err:
@@ -192,20 +235,58 @@ class ProxyHandler:
 
     def before(
         self,
-        priority: HandlerPriority | int = HandlerPriority.HIGHEST,
-    ):
-        def inner(func):
-            self.before_handlers[priority] = func
+        priority: Priority | int = Priority.HIGHEST,
+    ) -> Callable[[BeforeHandler], BeforeHandler]:
+        """Decorator to add a before handler to the proxy handler
+
+        :param priority: The priority of the handler which defines order of execution.
+            You can use the Priority enum to set the priority, or a simple integer. Keep in mind
+            that lower numbers mark a higher priority. Default is Priority.HIGHEST.
+        :returns: A decorator that adds the function to the before handlers collection
+        """
+
+        def inner(func: BeforeHandler):
+            self.before_handlers.add(priority=priority, value=func)
             return func
 
         return inner
 
     def after(
         self,
-        priority: HandlerPriority | int = HandlerPriority.HIGHEST,
-    ):
-        def inner(func):
-            self.after_handlers[priority] = func
+        priority: Priority | int = Priority.HIGHEST,
+    ) -> Callable[[AfterHandler], AfterHandler]:
+        """Decorator to add an after handler to the proxy handler
+
+        :param priority: The priority of the handler which defines order of execution.
+            You can use the Priority enum to set the priority, or a simple integer. Keep in mind
+            that lower numbers mark a higher priority. Default is Priority.HIGHEST.
+        :returns: A decorator that adds the function to the after handlers collection
+        """
+
+        def inner(func: AfterHandler):
+            self.after_handlers.add(priority=priority, value=func)
             return func
 
         return inner
+
+    def add_before(self, priority: Priority, func: BeforeHandler):
+        """Add a before handler to the proxy handler
+
+        :param priority: The priority of the handler which defines order of execution.
+            You can use the Priority enum to set the priority, or a simple integer. Keep in mind
+            that lower numbers mark a higher priority. Default is Priority.HIGHEST.
+        :param func: The function to add to the before handlers collection
+        :returns: None
+        """
+        self.before_handlers.add(priority, func)
+
+    def add_after(self, priority: Priority, func: AfterHandler):
+        """Add an after handler to the proxy handler
+
+        :param priority: The priority of the handler which defines order of execution.
+            You can use the Priority enum to set the priority, or a simple integer. Keep in mind
+            that lower numbers mark a higher priority. Default is Priority.HIGHEST.
+        :param func: The function to add to the after handlers collection
+        :returns: None
+        """
+        self.after_handlers.add(priority, func)
