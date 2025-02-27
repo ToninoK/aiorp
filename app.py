50ebe33e782e7cf1e974c9e9ef6546997ea28cff
ToninoK <tonino.kastelan@kiwi.com>
2025-02-27 12:56:49 +0100

    expand example

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from aiohttp import web
 from yarl import URL
 
 from proxy.context import ProxyContext
-from proxy.handler import ProxyHandler
+from proxy.handler import Priority, ProxyHandler
 from proxy.request import ProxyRequest
 from proxy.response import ProxyResponse
 
-pokeapi_context = ProxyContext(url=URL("https://pokeapi.co"), attributes={})
+pokeapi_context = ProxyContext(
+    url=URL("https://pokeapi.co"), attributes={"target": "pokeapi"}
+)
 
 handler = ProxyHandler(
     pokeapi_context,
@@ -19,14 +21,29 @@ handler = ProxyHandler(
 log = logging.getLogger(__name__)
 
 
-@handler.before()
-async def prepare(request: ProxyRequest):
-    log.info(request.headers)
+@handler.before(priority=Priority.HIGHEST)
+async def prepare_1(request: ProxyRequest):
+    log.info("I execute first")
+    log.info(f"Target: {request.proxy_attributes['target']}")
+
+
+@handler.before(priority=Priority.HIGH)
+async def prepare_2(request: ProxyRequest):
+    log.info("I execute second")
 
 
 @handler.after()
-async def process(response: ProxyResponse):
-    log.info(response.response.headers)
+async def process_1(response: ProxyResponse):
+    log.info("I execute third")
+
+
+async def manual_process_3(response: ProxyResponse):
+    log.info("I execute third also asynchronously")
+
+
+@handler.after(priority=Priority.HIGH)
+async def process_2(response: ProxyResponse):
+    log.info("I execute fourth")
 
 
 async def on_shutdown(app):
@@ -34,11 +51,15 @@ async def on_shutdown(app):
 
 
 application = web.Application()
+
+handler.add_after(priority=Priority.HIGHEST, func=manual_process_3)
 handler_routes = [
     web.get("/pokapi/pokemon/{name}", handler),
 ]
+
 application.router.add_routes(handler_routes)
 application.on_cleanup.append(on_shutdown)
+
 logging.basicConfig(level=logging.DEBUG)
 
 web.run_app(application)
